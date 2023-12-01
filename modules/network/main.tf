64defbc10a50b277c5b804e13050f2777ff16a3c
Sai Vishnu Edala <saivishnu.edala@gmail.com>
2023-12-01 10:52:27 -0600

    modified: ngw and rt resource

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -39,7 +39,7 @@ resource "aws_route_table" "pri_rt" {
  vpc_id = aws_vpc.main.id
  route {
  cidr_block = "0.0.0.0/0"
- gateway_id = aws_internet_gateway.ngw.id
+ gateway_id = aws_nat_gateway.ngw.id
   }
 }
 #PUBLIC SUBNET ASSOCIATION
